14dd9fe5f3f011e621bbc3132ebab0ef84b9834a
Constantin <constantin.chtanko@gmail.com>
2024-12-17 01:35:05 +0100

    Fraud service spring boot

--- fraud/src/main/java/com/lysero/fraud/FraudApplication.java
+++ fraud/src/main/java/com/lysero/fraud/FraudApplication.java
@@ -2,12 +2,14 @@ package com.lysero.fraud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.lysero.fraud")
 public class FraudApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FraudApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FraudApplication.class, args);
+    }
 
 }
--- fraud/src/test/java/com/lysero/fraud/FraudApplicationTests.java
+++ fraud/src/test/java/com/lysero/fraud/FraudApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class FraudApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
