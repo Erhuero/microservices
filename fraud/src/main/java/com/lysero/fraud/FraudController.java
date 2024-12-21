a7e66e479cea1a1b2bf8c84050300415c799dade
Constantin <constantin.chtanko@gmail.com>
2024-12-21 01:36:40 +0100

    Eureka server implementation

--- fraud/src/main/java/com/lysero/fraud/FraudController.java
+++ fraud/src/main/java/com/lysero/fraud/FraudController.java
@@ -1,6 +1,7 @@
 package com.lysero.fraud;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
@@ -8,6 +9,7 @@ import java.util.UUID;
 @RestController
 @RequestMapping("api/v1/fraud-check")
 @AllArgsConstructor
+@Slf4j
 public class FraudController {
 
     private final FraudCheckService fraudCheckService;
@@ -17,6 +19,8 @@ public class FraudController {
             @PathVariable("playerId") UUID playerId) {
         boolean isFraudulentCustomer = fraudCheckService.
                 isFraudulentCustomer(playerId);
+        log.info("fraud check request for customer {}", playerId);
+
         return new FraudCheckResponse(isFraudulentCustomer);
     }
 }
