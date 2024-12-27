50f7633b889e46e7a0998c1138160a67d81a15f2
Constantin <constantin.chtanko@gmail.com>
2024-12-27 21:37:28 +0100

    Gateway yaml

--- apigw/src/main/java/com/lysero/apigw/ApiGWApplication.java
+++ apigw/src/main/java/com/lysero/apigw/ApiGWApplication.java
@@ -2,12 +2,14 @@ package com.lysero.apigw;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-public class ApigwApplication {
+@EnableEurekaClient
+public class ApiGWApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ApigwApplication.class, args);
+        SpringApplication.run(ApiGWApplication.class, args);
     }
 
 }
