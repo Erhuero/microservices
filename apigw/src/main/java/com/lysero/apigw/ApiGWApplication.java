ed5159ff92e3343a10f607e9aae003b1c6155df1
Constantin <constantin.chtanko@gmail.com>
2024-12-29 00:44:52 +0100

    Amqp configuration

--- apigw/src/main/java/com/lysero/apigw/ApiGWApplication.java
+++ apigw/src/main/java/com/lysero/apigw/ApiGWApplication.java
@@ -2,10 +2,12 @@ package com.lysero.apigw;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EntityScan(basePackages = "com.lysero.amqp")
 public class ApiGWApplication {
 
     public static void main(String[] args) {
